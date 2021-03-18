b40102bda56ce66395431f2cd7ed1f4171681e52
pawel02 <pawel.bes13@gmail.com>
2021-03-18 17:04:05 +0000

    Added playing of youtube videos

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from utility import message_handler
 
 client = discord.Client()
+
 text_channel_list = []
 handler = message_handler(client, text_channel_list)
 
@@ -13,21 +14,80 @@ async def on_ready():
         for channel in guild.text_channels:
             text_channel_list.append(channel)
     
-    handler.update_channel_list(text_channel_list)
+    #handler.update_channel_list(text_channel_list)
+    
+    #await handler.search()
 
-    await handler.write_to_all_channels('Bought to you by Jonny sins. An engineer, a doctor, a real life astrounaut and a father to us all. https://img.mensxp.com/media/content/2019/Sep/johnny-sins-trolls-former-pakistan-envoy-abdul-basit1200-1567585868_1200x900.jpg')
+    #await handler.write_to_all_channels('Bought to you by Jonny sins. An engineer, a doctor, a real life astrounaut and a father to us all. https://img.mensxp.com/media/content/2019/Sep/johnny-sins-trolls-former-pakistan-envoy-abdul-basit1200-1567585868_1200x900.jpg')
 
     #write a list of commands
-    await handler.help()
+    #await handler.help()
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    await handler.handle_message(message.content, message.channel)
+    await handler.handle_message(message.content, message.channel, message.author.voice.channel)
 
 token = ""
 with open("token.txt") as file:
     token = file.read()
 client.run(token)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# song_queue = []
+# FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+# YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+
+# #Search videos from key-words or links
+# def search(arg):
+#     try: requests.get("".join(arg))
+#     except Exception: arg = " ".join(arg)
+#     else: arg = "".join(arg)
+#     with YoutubeDL(YDL_OPTIONS ) as ydl:
+#         info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+        
+#     return {'source': info['formats'][0]['url'], 'title': info['title']}
+
+# #Plays the next song in the queue
+# def play_next(ctx):
+#     voice = get(bot.voice_clients, guild=ctx.guild)
+#     if len(song_queue) > 1:
+#         del song_queue[0]
+#         voice.play(discord.FFmpegPCMAudio(song_queue[0][source], **FFMPEG_OPTIONS), after=lambda e: play_next(ctx))
+#         voice.is_playing()
+
+# @bot.command()
+# async def play(ctx, *arg):
+#     print("hi")
+#     channel = ctx.message.author.voice.channel
+
+#     if channel:
+#         voice = get(bot.voice_clients, guild=ctx.guild)
+#         song = search(arg)
+#         song_queue.append(song)
+
+#         if voice and voice.is_connected():
+#             await voice.move_to(channel)
+#         else: 
+#             voice = await channel.connect()
+
+#         if not voice.is_playing():
+#             voice.play(discord.FFmpegPCMAudio(song_queue[0][source], **FFMPEG_OPTIONS), after=lambda e: play_next(ctx))
+#             voice.is_playing()
+#         else:
+#             await ctx.send("Added to queue")
+#     else:
+#         await ctx.send("You're not connected to any channel!")
--- utility.py
+++ utility.py
@@ -5,6 +5,9 @@ import discord
 import random
 import base64
 
+from youtube_dl import YoutubeDL
+import requests
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -19,7 +22,7 @@ class message_handler:
         self.driver.get('https://www.google.ca/imghp?hl=en&tab=ri&authuser=0&ogbl')
 
         #first dummy search as the xpath is different afterwards
-        box = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
+        box = self.driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
         box.send_keys(self.keywords)
         box.send_keys(Keys.ENTER)
 
@@ -29,9 +32,17 @@ class message_handler:
 /get - will get the image based on the current search
 /clear amount - will delete the past messages with the amount specified
 """
+        self.srcs = []
 
-    async def help(self, channel):
-        await channel.send(self.help_message)
+        #all the music related stuff
+        self.is_playing = False
+
+        # 2d array containg [channel, url]
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = ""
 
     async def help(self):
         await self.write_to_all_channels(self.help_message)
@@ -40,18 +51,8 @@ class message_handler:
         self.channels = new_channels
 
     async def get_image(self, channel):
-        box = self.driver.find_element_by_xpath('//*[@id="REsRA"]')
-        box.clear()
-        box.send_keys(self.keywords)
-        box.send_keys(Keys.ENTER)
-
-        images = self.driver.find_elements_by_css_selector(".rg_i")
-        srcs = []
-        for image in images:
-            srcs.append(image.get_property("src"))
-
         #select a random image
-        img = srcs[random.randint(0, len(srcs) - 1)]
+        img = self.srcs[random.randint(0, len(self.srcs) - 1)]
         if len(img) > 2000:
             img_data = img
             # delete the header
@@ -79,14 +80,68 @@ class message_handler:
             amount = 5
         await channel.purge(limit=amount)
 
-    async def handle_message(self, msg, channel):
+    async def search(self):
+        box = self.driver.find_element_by_xpath('//*[@id="REsRA"]')
+        box.clear()
+        box.send_keys(self.keywords)
+        box.send_keys(Keys.ENTER)
+
+        images = self.driver.find_elements_by_css_selector(".rg_i")
+        self.srcs = []
+        for image in images:
+            self.srcs.append(image.get_property("src"))
+
+    #searching the item on youtube
+    def search_yt(self, item):
+        try: requests.get(item)
+        except Exception: pass
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
+            
+        return {'source': info['formats'][0]['url'], 'title': info['title']}
+
+    def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(self.music_queue[0][0], download=False)
+            m_url = info['formats'][0]['url']
+
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    # infinite loop checking 
+    async def play_music(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(self.music_queue[0][0], download=False)
+            m_url = info['formats'][0]['url']
+
+            try: self.vc = await self.music_queue[0][1].connect()
+            except Exception: pass
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    async def handle_message(self, msg, channel, voice_channel):
         #figure out whether its a valid command
         if msg.startswith("/search"):
             self.keywords = msg[8:]
+            await self.search()
         if msg.startswith("/get"):
            await self.get_image(channel)
         if msg.startswith("/help"):
-            await self.help(channel)
+            await channel.send(self.help_message)
         if msg.startswith("/clear"):
             try:
                 amount = int(msg[6:])
@@ -94,7 +149,17 @@ class message_handler:
             except Exception:
                 await self.clear(5, channel)
 
-
+        #playing the music in the queue
+        if msg.startswith("/play"):
+            #make sure that the voice_channel exists
+            if voice_channel is None:
+                await channel.send("Connect to a voice channel!")
+            else:
+                await channel.send("Song added to the queue")
+                song = self.search_yt(msg[6:])
+                self.music_queue.append([song['source'], voice_channel])
+                if self.is_playing == False:
+                    await self.play_music()
 
     async def write_to_all_file(self, f):
         for channel in self.channels:
